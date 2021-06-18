bf193cad1140d37e959cd7da6e949bcf6f189f3f
vineesha-guptha <69143091+vineesha-guptha@users.noreply.github.com>
2021-06-18 14:55:51 +0530

    Update ThreadbyThread.java

--- JAVA-LAB/ThreadbyThread.java
+++ JAVA-LAB/ThreadbyThread.java
@@ -1,1 +1,31 @@
+class A extends Thread
+{
+public A(String s)
+{
+super(s);
+}
+public void run()
+{
+for (int i=0;i<=5;i++)
+{
+System.out.println(getName());
+}
+}
+}
 
+
+class ThreadbyThread
+{
+public static void main(String args[])
+{
+A a1 = new A("sreeja");
+A a2 = new A ("rishi");
+A a3 = new A ("sahit");
+a1.start();
+a2.start();
+a3.start();
+
+System.out.println(" Hello");
+
+}
+}
