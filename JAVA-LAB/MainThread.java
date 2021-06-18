357c4d2d260a1f04a1f577b01e13ad9549998ff0
vineesha-guptha <69143091+vineesha-guptha@users.noreply.github.com>
2021-06-18 14:42:19 +0530

    Update MainThread.java

--- JAVA-LAB/MainThread.java
+++ JAVA-LAB/MainThread.java
@@ -1,1 +1,8 @@
-
+class MainThread
+{
+public static void main (String args[])
+{
+System.out.println("Thread Name = " + Thread.currentThread().getName());
+System.out.println(" Priority = " + Thread.currentThread().getPriority());
+}
+}
