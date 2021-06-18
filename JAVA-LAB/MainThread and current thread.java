eccf8b34b0c959b362e36e1f079a99b11f3a3993
vineesha-guptha <69143091+vineesha-guptha@users.noreply.github.com>
2021-06-18 13:00:14 +0530

    Update MainThread and current thread.java

--- JAVA-LAB/MainThread and current thread.java
+++ JAVA-LAB/MainThread and current thread.java
@@ -1,1 +1,11 @@
+public class MainThread0
+{
+public static void main(String args[])
+{
+Thread t = Thread.currentThread();
 
+System.out.println(" Current Thread Name : " + t);
+t.setName("Gabber");
+System.out.println("After Changing the Name : " +t);
+}
+}
