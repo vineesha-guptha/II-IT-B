ec851e82603b0ed849e6237e342118494b644b2b
vineesha-guptha <69143091+vineesha-guptha@users.noreply.github.com>
2021-06-18 12:42:16 +0530

    Update LeapYear.java

--- JAVA-LAB/BASIC PROGRAMS/LeapYear.java
+++ JAVA-LAB/BASIC PROGRAMS/LeapYear.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+class LeapYear{
+public static void main(String[] args){
+int year;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter any year: ");
+year = sc.nextInt();
+sc.close();
+boolean isLeap = false;
+if(year % 4 ==0){
+if(year % 100 == 0){
+if(year % 400 == 0)
+isLeap = true;
+else
+isLeap = false;
+}
+else
+isLeap = true;
+}
+else{
+isLeap = false;
+}
+if(isLeap == true)
+System.out.println(year+ " is a leap year");
+else
+System.out.println(year+ " is not a leap year");
+}
+}
